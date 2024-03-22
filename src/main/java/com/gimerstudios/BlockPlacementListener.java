c4773d23c7ccf4ca1fd39fd36b9e50719819d2bd
Mincraftgimer <gimerstudios@gmail.com>
2024-03-22 17:00:51 -0400

    Add Code

--- src/main/java/com/gimerstudios/BlockPlacementListener.java
+++ src/main/java/com/gimerstudios/BlockPlacementListener.java
@@ -1,2 +1,35 @@
-package com.gimerstudios;public class BlockPlacementListener {
+package com.gimerstudios;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.util.Vector;
+
+public class BlockPlacementListener implements Listener {
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        for (MetadataValue meta : player.getMetadata("ClientVersion")) {
+            if (meta.asString().contains("Java")) {
+                // Get the block's location
+                Location blockLocation = event.getBlockPlaced().getLocation();
+
+                // Get the player's location and direction
+                Location playerLocation = player.getLocation();
+                Vector playerDirection = playerLocation.getDirection();
+
+                // Create a vector from the player to the block
+                Vector toBlock = blockLocation.subtract(playerLocation).toVector();
+
+                // Check if the block is being placed in front of the player
+                if (playerDirection.dot(toBlock) > 0) {
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
 }
--- src/main/java/com/gimerstudios/GeyserBridgeFix.java
+++ src/main/java/com/gimerstudios/GeyserBridgeFix.java
@@ -6,12 +6,15 @@ public final class GeyserBridgeFix extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("GeyserBridgeFix has been enabled.");
+        getLogger().info("DEV BUILD 0.1");
+        getServer().getPluginManager().registerEvents(new BlockPlacementListener(), this);
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("GeyserBridgeFix has been disabled.");
+        getLogger().info("DEV BUILD 0.1");
     }
 }
